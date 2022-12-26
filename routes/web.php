87f2579855634ae124ef0bfcd50ed23530fbe386
Murat Ashimov <muratashimov111@gmail.com>
2022-12-26 16:31:49 +0500

    Fixed routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
+
 use Illuminate\Support\Facades\Route;
 
-Route::resource('categories', CategoryController::class)
-    ->only(['index', 'show'])
-    ->parameter('category', 'slug');
